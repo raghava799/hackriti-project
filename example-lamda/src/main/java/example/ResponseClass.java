cddd261aec9e840c4c2c48b00e00af07595bdbf7
raghavendara <raghavendar.arra@alacriti.com>
2018-11-23 21:28:03 +0530

    adding sample lambda function

---
+++ example-lamda/src/main/java/example/ResponseClass.java
@@ -0,0 +1,17 @@
+package example;
+
+public class ResponseClass {
+String greetings;
+
+public ResponseClass(String greetings) {
+    this.greetings = greetings;
+}    
+public ResponseClass() {
+}  
+public String getGreetings() {
+    return greetings;
+}
+public void setGreetings(String greetings) {
+    this.greetings = greetings;
+}
+}
